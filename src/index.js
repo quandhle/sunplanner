b2f6efb7fb0b17fc339d9971ddd6aa1592461360
Quan Le <quandhle@gmail.com>
2019-06-14 21:49:33 -0700

    changed folder structure of app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './src/App';
+import App from './components/app'
 
 ReactDOM.render(
     <Router>
